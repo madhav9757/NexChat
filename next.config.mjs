06a055258f92068c63e275199e5df09c8a48a14a
madhav9757 <madhavsemwal9@gmail.com>
2025-09-01 11:22:56 +0530

    refactor: comment out unused redirects function in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "/conversation",
-                permanent: true,
-            },
-        ];
-    }
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: "/",
+    //             destination: "/conversation",
+    //             permanent: true,
+    //         },
+    //     ];
+    // }
 };
 
 export default nextConfig;
